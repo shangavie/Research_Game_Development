3068d390547ec7218c0e6fdf3cd59c1f79ae64e8
it16167124@my.sliit.lk <it16167124@my.sliit.lk>
2020-09-23 08:34:20 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from flask import Flask, jsonify, request
 import pandas as pd
 import numpy as np
@@ -16,26 +10,26 @@ app = Flask(__name__)
 def hello():
     return "hey"
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predictQuestion', methods=['POST'])
 def predict():
-        lr = joblib.load("model.pkl")
-        if lr:
-        try:
-            json = request.get_json()
-            model_columns = joblib.load("model_cols.pkl")
-            temp=list(json[0].values())
-            vals=np.array(temp)
-            prediction = lr.predict(temp)
-            print("here:",prediction)        
-            return jsonify({'prediction': str(prediction[0])})
+	lr = joblib.load("questionmodel.pkl")
+	if lr:
+		try:
+			json = request.get_json()
+			model_columns = joblib.load("questionmodel_cols.pkl")
+			#temp=list(json[0].values())
+			vals=np.array([[json[0][0]['CurrentQuestion'],json[0][0]['TimeTakenToAnswer'],json[0][0]['AnswerStatus'],json[0][0]['TimeGiven']]])
+			predictionQuestion = lr.predict(vals)
+			print("here:",predictionQuestion)
+            
+			return jsonify({'prediction': str(predictionQuestion)})
 
-        except:        
-            return jsonify({'trace': traceback.format_exc()})
-    else:
-        return ('No model here to use')
+		except:        
+			return jsonify({'trace': traceback.format_exc()})
+	else:
+		return ('No model here to use')
     
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
